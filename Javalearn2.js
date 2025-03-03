c8156548681fca9fd65720d915a640ff5fbac08b
Adarshtiwari-2211 <adarshtiwari.aohr@gmail.com>
2025-03-03 15:46:07 +0530

    Added my project

---
+++ Javalearn2.js
@@ -0,0 +1,11 @@
+function checkEvenOdd(num) {
+    if (num % 2 === 0) {
+      console.log(num + " is even.");
+    } else {
+      console.log(num + " is odd.");
+    }
+  }
+  
+  checkEvenOdd(10); // Output: 10 is even.
+  checkEvenOdd(7);  // Output: 7 is odd.
+  
