955470b2aef03628aa1fe0f7fa8a5da2664566c1
Adarshtiwari-2211 <adarshtiwari.aohr@gmail.com>
2025-03-03 15:18:26 +0530

    Update Javalearn.java

--- Javalearn.java
+++ Javalearn.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class AddTwoNumbers {
+public class LargestNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -10,8 +10,12 @@ public class AddTwoNumbers {
         System.out.print("Enter second number: ");
         int num2 = scanner.nextInt();
 
-        int sum = num1 + num2;
-        System.out.println("Sum: " + sum);
+        System.out.print("Enter third number: ");
+        int num3 = scanner.nextInt();
+
+        int largest = (num1 > num2) ? (num1 > num3 ? num1 : num3) : (num2 > num3 ? num2 : num3);
+
+        System.out.println("Largest number is: " + largest);
 
         scanner.close();
     }
